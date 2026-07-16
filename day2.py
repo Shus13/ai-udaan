c5bfe3a217f26f55e7312b30817b17992ded19d8
Shus13 <sushitkarki234@gmail.com>
2026-07-16 23:00:38 +0545

    day 2

---
+++ day2.py
@@ -0,0 +1,17 @@
+# Intro to programming language : Computer lai instruct garney way programming language ho; jastai C, C++, C#, python, java, javascript; Different scenario ko lagi different languages use garinxa
+# Web apps ko lagi JS ma packages sajilai paiyeney vayeka ley JS dherai use hunxa
+# Similarly, for AI ani ML, python packages are easily available so, yei resource availability ko karan, we use python for AI/ML
+# Python intreprete language ho means that ki pythone execute line by line hunxa
+
+# Python installation and setup.
+
+# Datatypes : It is simply the classification of data; jasley data kun type ko ho tha hunxa ni tesko memory storage, data lai variabe ma store garinxa. 
+# eg :
+
+a = 25                      #This is integer
+name = "Sushit"             #Yo chai string vayo, string lai inverted comma ma rakhinxa
+b = 2.55                    #this is float
+
+# writing convention in python is snake_case
+
+is_learning = True          #This is boolean
