765836a792ccaa846d6599422e99c010563088b8
Shus13 <sushitkarki234@gmail.com>
2026-07-16 22:49:51 +0545

    first commit

---
+++ day1.py
@@ -0,0 +1,19 @@
+# Artificial Intelligence : Machine lai human le jastai think garna, learn garna, decision lina sakney banauney technology
+
+# Machine Learning : ML is the branch of AI, jasly data liyeko aadhar ma aafai learn garney ani decision lina sakney banxa; eg: Email filtering, Recommendations,
+
+# Types
+# Supervised learning : machine lai labelled(input ko yo output hunxa) data diyera train garayinxa; 
+# Un-Supervised learning : machine lai raw/unlabelled data ma train garainxa
+# Semi-Supervised learning : It is the mixed up of both supervised and unsupervised; labelled and unlabelled data
+# Reinforcement learning : machine ley error ani experience sangai learn gardai janxa, There is a rule called "Reward and Punishment"; Jaba better result lyauxa it get's reward points ani error badyo vaney reward points ghatxa eg: Chess playing machine, Self-driving cars
+
+# Some algorithms : 
+# Linear regression : House evaluation - SL
+# Logistic regression : Yes/No - SL
+# Support Vector machine : Hyperlink - SL
+# K means Clustering : Classification/Grouping in K - USL
+# Graph based Model : Network - SSL
+# Deep Q-Network : Neural Network - RL
+
+# Deep Learning : DL is the modern branch of ML, jasley Artificial Neural Network use garxa for depth and detailed learning. It needs Big data and advanced GPU, ANN ma layers hunxa, input, hidden, output (just like in back propagation problem); eg: LLMs, Digital assistance
