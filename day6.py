a5f95c88bdae45767f217749d64072ad995554a6
Shus13 <sushitkarki234@gmail.com>
2026-07-22 08:26:29 +0545

    day 6

---
+++ day6.py
@@ -0,0 +1,29 @@
+# Data Thinking
+# Data is te fuel of AI, Data thinking means that using data rather than random prediction in order to solve a problem or form strategy or decision making. 
+# Things to need for data thinking
+# Dataset is must
+# Data format:
+# Tabular format: Data are in row and column, json format: data are in key value pair, CSV format: Plain text, XML format: tag to define data
+
+# Data Types:
+# Numeric: numbers, Categorical: classified data, Boolean: True or False, Textual: text/string, Image: Images only .png, Audio/Video: videos file, .mp3
+
+# Features are the input data given and The output/outcome form that input is called labels
+# We use 80% of data for training and 20% od data for testing
+
+# In this process data are collected, review, segreggate, clean, prepare and feed
+# These steps can be done using the python open source library or we can write everything ourself which will be time consuming
+# To use library we use pip to install and manage packages.
+
+num = [10,20,30,40,50]
+
+num2 = map(lambda x:x+5, num)
+print(list(num2))
+
+# Pandas: Yo Open Source Libarary ho python ko which is used for computation of numbers
+
+# Numpy: This is also the Open Source Library of python for manipulation and analysis of data
+# To install packages: pip install pkg_name; Eg: pip install numpy
+# Every packages have version like numpy 2.5.1
+# Version is also known as Semantic versioning/semver. 
+# Version is in major.minor.patch format
